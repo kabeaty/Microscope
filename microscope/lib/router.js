a418b6534475c9aeb74e219aeeebc9871612694c
Kathryn Beaty <kabeaty@gmail.com>
2014-03-08 18:16:24 -0600

    Add post submit path and template

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -10,5 +10,11 @@ Router.map(function() {
   this.route('postPage', {
     path: '/posts/:_id',
     data: function() { return Posts.findOne(this.params._id); }
+    });
+
+  this.route('postSubmit', {
+    path: '/submit'
   });
 });
+
+

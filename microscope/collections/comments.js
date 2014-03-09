2a14bf4d107df27a7a903975841c65bdea079e12
Kathryn Beaty <kabeaty@gmail.com>
2014-03-09 18:23:31 -0500

    Add ability to submit comments, and validation

--- microscope/collections/comments.js
+++ microscope/collections/comments.js
@@ -1,1 +1,25 @@
 Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+  comment: function(commentAttributes) {
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+
+    if (!user)
+      throw new Meteor.Error(401, "Log in to comment");
+
+    if (!commentAttributes.body)
+      throw new Meteor.Error(422, "Write something");
+
+    if (!post)
+      throw new Meteor.Error(422, "You have to comment on a post");
+
+    comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+      userId: user._id,
+      author: user.username,
+      submitted: new Date().getTime()
+    });
+
+    return Comments.insert(comment);
+  }
+});
